80b588b71c97e2ad80d533ee4c02075884a180f7
Guilherme Linden <guilinden@tca.local>
2016-06-15 09:18:54 -0300

    added restricted permission

--- deletar_categoria.php
+++ deletar_categoria.php
@@ -1,4 +1,13 @@
 <?php
+  session_start();
+  if(isset($_SESSION['user'])){
+    echo "Welcome " . $_SESSION['user'];
+    $id = session_id();
+  }
+  else{
+    echo "<script language='javascript' type='text/javascript'>alert('Voce de estar logado');window.location.href='index.php';</script>";
+  }
+ 
   $connection = mysql_connect("localhost","root","");
   mysql_select_db("blog",$connection);
   $id = $_GET['id'];
